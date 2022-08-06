96d6b555ad13b9730139764a8bdbcddb7bf7d537
karimbaggari <karimbaggari8@gmail.com>
2022-08-06 22:36:00 +0100

    update in searchBar

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import { useState } from 'react';
-import Productsgrid from './ProductsGrid'
 export default function SearchInput(props) {
 
     return (
         <>
-          
             <hr className=" w-screen bg-gray-200 my-6 -mb-4 mt-4" style={{ position: "relative", width: "72" + "%", left: "14" + "%" }} />
         </>
     )
